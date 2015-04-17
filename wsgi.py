cae01dbe2ab1ac2088a106e27203f7c5fa7bbf9b
Andrej Antonov <polymorphm@gmail.com>
2015-04-17 22:07:02 +0300

    created 'wsgi.py' as host router

---
+++ wsgi.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- mode: python; coding: utf-8 -*-
+
+assert str is not bytes
+
+import httpdistortion_wsgi as _httpdistortion_wsgi
+import pnzact_wsgi as _pnzact_wsgi
+
+_application_by_host_dict = {
+    'www.httpdistortion.org': _httpdistortion_wsgi.application,
+    'www.pnzact.ru': _httpdistortion_wsgi.application,
+}
+
+def application(environ, start_response):
+    host = environ.get('HTTP_HOST')
+    
+    selected_application = _application_by_host_dict.get(host)
+    
+    if selected_application is None:
+        response_body = 'no application for this host'
+        
+        start_response('404 Not Found', [
+            ('Content-Type', 'text/plain;charset=utf-8'),
+        ])
+        
+        return response_body.encode(),
+    
+    return selected_application(environ, start_response)
+
+#
+# Below for testing only
+#
+if __name__ == '__main__':
+    from wsgiref import simple_server as _simple_server
+    _httpd = _simple_server.make_server('localhost', 8051, application)
+    _httpd.serve_forever()
